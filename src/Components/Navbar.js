d72f5d9a362c13d05224536711cc43cdb57e83c2
ManishChaudhari-11 <chaudharimk2001@gmail.com>
2023-08-02 14:03:10 +0530

    Edited Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -11,7 +11,7 @@ export default function Navbar(props) {
         {/* <Link className="navbar-brand" to="/">
           {props.title}
         </Link> */}
-        <a className="navbar-brand" href="/">
+        <a className="navbar-brand" href="/TextUtils-React/">
           {props.title}
         </a>
         <button
@@ -31,7 +31,11 @@ export default function Navbar(props) {
               {/* <Link className="nav-link active" aria-current="page" to="/">
                 Home
               </Link> */}
-              <a className="nav-link active" aria-current="page" href="/">
+              <a
+                className="nav-link active"
+                aria-current="page"
+                href="/TextUtils-React/"
+              >
                 Home
               </a>
             </li>
@@ -39,7 +43,7 @@ export default function Navbar(props) {
               {/* <Link className="nav-link" to="/about">
                 {props.aboutText}
               </Link> */}
-              <a className="nav-link" href="/">
+              <a className="nav-link" href="/TextUtils-React/">
                 {props.aboutText}
               </a>
             </li>
